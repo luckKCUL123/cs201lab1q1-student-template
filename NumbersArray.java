25a9203415c202997c2c09a6396703db4cd9b3e7
luckKCUL123 <luckkcul.who@gmail.com>
2026-09-05 23:14:56 +0800

    q1

--- NumbersArray.java
+++ NumbersArray.java
@@ -20,6 +20,43 @@ public class NumbersArray {
     }
 
     // Write your methods here
+    public static int findMax(Integer[] input){
+        int max = input[0];
+        for (int i = 1; i < input.length; i++){
+            if (input[i] > max){
+                max = input[i];
+            }
+        }
+        return max;
+    }
+
+    public static Integer[] findDuplicates(Integer[] input){
+        List<Integer> result = new ArrayList<>();
+        Map<Integer, Integer> count = new HashMap<>();
+        for (Integer num : input){
+            count.put(num, count.getOrDefault(num,0)+1);
+        }
+        for (Integer key : count.keySet()){
+            if (count.get(key) > 1){
+                result.add(key);
+            }
+        }
+        return result.toArray(new Integer[0]);
+    }
+
+    public static Integer[] findUnique(Integer[] input){
+        List<Integer> result = new ArrayList<>();
+        Map<Integer, Integer> count = new HashMap<>();
+        for (Integer num : input){
+            count.put(num, count.getOrDefault(num,0)+1);
+        }
+        for (Integer key : count.keySet()){
+            if (count.get(key).equals(1)){
+                result.add(key);
+            }
+        }
+        return result.toArray(new Integer[0]);
+    }
     
 }
 
